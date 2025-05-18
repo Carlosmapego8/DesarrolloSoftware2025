67489b272693008a5df9dafe0caad1791e6a6c6b
Cristóbal Merino <cristobal.merino.saez@gmail.com>
2025-05-18 22:21:07 +0200

    Añadidos tests Carlitos

--- Practica3/test/bankService_test.dart
+++ Practica3/test/bankService_test.dart
@@ -13,33 +13,5 @@ void main() {
       expect(() => bankService.withdraw('TEST', account.number, 50),
           throwsA(isA<StateError>()));
     });
-
-    test('Lista inicial de cuentas está vacía', () {
-      final bankService = BankService();
-
-      expect(bankService.listAllAccounts(), isEmpty);
-    });
-
-    test('deposit aumenta el saldo de la cuenta', () {
-      final bankService = BankService();
-      bankService.registerUser("Alice");
-      final account = bankService.createAccount("Alice");
-      bankService.deposit("Alice", account.number, 100.0);
-
-      expect(bankService.getBalance(account.number), 100.0);
-    });
-
-    test('transfer mueve fondos correctamente', () {
-      final bankService = BankService();
-      bankService.registerUser("Bob");
-      final from = bankService.createAccount("Bob");
-      final to = bankService.createAccount("Bob");
-      bankService.deposit("Bob", from.number, 200.0);
-      bankService.transfer("Bob", from.number, to.number, 75.0);
-
-      expect(bankService.getBalance(from.number), 125.0);
-      expect(bankService.getBalance(to.number), 75.0);
-    });
-
   });
 }
