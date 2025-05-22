d6b536e7722be9e7301dd76c3673b3186d3040d6
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2025-05-22 10:02:14 +0200

    Añadido el campo name

--- Practica4/SpendWise/lib/models/transaction.dart
+++ Practica4/SpendWise/lib/models/transaction.dart
@@ -5,11 +5,13 @@ class Transaction {
   String category;
   DateTime date;
   TransactionType type;
+  String name;
 
   Transaction({
     required this.amount,
     required this.category,
     required this.date,
     required this.type,
+    required this.name,
   });
 }
