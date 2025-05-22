79e2c888127b37ea59670c06116a96b15532f5ef
Carlos Manuel Perales Gómez <130476574+Carlosmapego8@users.noreply.github.com>
2025-05-22 10:01:42 +0200

    Añadido el campo name

--- Practica4/SpendWise/lib/factory/transaction_factory.dart
+++ Practica4/SpendWise/lib/factory/transaction_factory.dart
@@ -1,29 +1,31 @@
 import '../models/transaction.dart';
 
 abstract class TransactionFactory {
-  Transaction createTransaction(double amount, String category, DateTime date);
+  Transaction createTransaction(double amount, String category, DateTime date, String name);
 }
 
 class ExpenseFactory implements TransactionFactory {
   @override
-  Transaction createTransaction(double amount, String category, DateTime date) {
+  Transaction createTransaction(double amount, String category, DateTime date, String name) {
     return Transaction(
       amount: amount,
       category: category,
       date: date,
       type: TransactionType.expense,
+      name: name,
     );
   }
 }
 
 class IncomeFactory implements TransactionFactory {
   @override
-  Transaction createTransaction(double amount, String category, DateTime date) {
+  Transaction createTransaction(double amount, String category, DateTime date, String name) {
     return Transaction(
       amount: amount,
       category: category,
       date: date,
       type: TransactionType.income,
+      name: name,
     );
   }
 }
